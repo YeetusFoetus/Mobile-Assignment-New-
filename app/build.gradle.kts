ac22d90c750c9bf1a9e511f09071344a83403222
Elvis Ong Ys <114889921+elvisoys@users.noreply.github.com>
2026-09-06 00:55:08 +0800

    Upgrade the versions of libraries on Gradle.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
-    kotlin("plugin.serialization") version "2.0.0"
+    kotlin("plugin.serialization") version "2.4.0"
 }
 
 android {
@@ -54,9 +54,10 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.6.0")
 
     // Supabase
-    implementation("io.github.jan-tennert.supabase:postgrest-kt:2.5.0")
-    implementation("io.github.jan-tennert.supabase:gotrue-kt:2.5.0")
-    implementation("io.ktor:ktor-client-android:2.3.11")
+    implementation(platform("io.github.jan-tennert.supabase:bom:3.8.0"))
+    implementation("io.github.jan-tennert.supabase:postgrest-kt:3.8.0")
+    implementation("io.github.jan-tennert.supabase:gotrue-kt:2.6.1")
+    implementation("io.ktor:ktor-client-android:3.5.2")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
 
     // For permissions
