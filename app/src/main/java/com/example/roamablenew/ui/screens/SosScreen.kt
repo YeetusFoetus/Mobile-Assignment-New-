59467b5d8fec3faa73726258f65d59d96f4d398a
Elvis Ong Ys <114889921+elvisoys@users.noreply.github.com>
2026-09-06 19:43:35 +0800

    Loads the SOS submodule on 'SosScreen()' composable function.

--- app/src/main/java/com/example/roamablenew/ui/screens/SosScreen.kt
+++ app/src/main/java/com/example/roamablenew/ui/screens/SosScreen.kt
@@ -2,8 +2,10 @@ package com.example.roamablenew.ui.screens
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import com.example.roamablenew.sos.navigation.SOSNavigationGraph
 
 @Composable
 fun SosScreen() {
-    Text("Coming Soon")
+    SOSNavigationGraph(LocalContext.current)
 }
